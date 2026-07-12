a1aa8b3dfdee52a18db6ee5de2c482f15b1d1f77
Frederic-Champion <fredichampix@gmail.com>
2026-07-12 17:29:08 +0200

    installation vite et exercice

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Section exercices à importer et commenter/décommenter dès que l'on souhaite afficher le composant avec App()
+
+import GestionMontures from "./components-exercices/GestionMontures";
+
+function App() {
+  return <GestionMontures />;
+  //   return <exercice 2 />
+  //   return <exercice 3 />
+  //   ...
+}
+
+export default App;
