92969675b2b7c5ced47d86cd4771ae20fa66e5a7
Fred <fredichampix@gmail.com>
2026-08-25 14:00:17 +0200

    exerice liftingStateUp

--- src/components-exercices/Brouillon-TSX.tsx
+++ src/components-exercices/Brouillon-TSX.tsx
@@ -1,6 +1,67 @@
-function Brouillon2() {
-  null;
-  return <h1>Brouillon2</h1>;
+import { useState } from "react";
+
+interface Client {
+  nom: string;
+  dossier: string;
 }
 
-export default Brouillon2;
+interface SaisieClientProps {
+  onEnvoyer: (client: Client) => void;
+}
+
+function SaisieClient({ onEnvoyer }: SaisieClientProps) {
+  const [nom, setNom] = useState("");
+  const [dossier, setDossier] = useState("");
+
+  function envoyer(e: React.SubmitEvent) {
+    e.preventDefault();
+    const data: Client = {
+      nom: nom,
+      dossier: dossier,
+    };
+    onEnvoyer(data);
+  }
+
+  return (
+    <div>
+      <form onSubmit={envoyer}>
+        <div>
+          <label htmlFor="nom">Tape le nom complet</label>
+          <input type="text" value={nom} onChange={(e) => setNom(e.target.value)} id="nom" placeholder="Nom Complet" />
+        </div>
+        <div>
+          <label htmlFor="dossier">Numéro de dossier</label>
+          <input
+            type="text"
+            value={dossier}
+            onChange={(e) => setDossier(e.target.value)}
+            id="dossier"
+            placeholder="Dossier n°"
+          />
+        </div>
+        <button type="submit">ajouter</button>
+      </form>
+    </div>
+  );
+}
+
+function PageClient() {
+  const [data, setData] = useState({ nom: "", dossier: "" });
+
+  function recuperer(info: Client) {
+    setData(info);
+  }
+
+  return (
+    <>
+      <SaisieClient onEnvoyer={recuperer} />
+      <div>
+        {data.nom} - {data.dossier}
+      </div>
+    </>
+  );
+}
+
+export default function Brouillon2() {
+  return <PageClient />;
+}
